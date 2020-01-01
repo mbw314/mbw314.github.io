be2cf9a64ceffb3446a697aa00b264045e6bbea3
Michael Bradford Williams <michael@tvtime.com>
2019-12-31 17:17:24 -0800

    more cleanup

--- js/canvasUtil.js
+++ js/canvasUtil.js
@@ -1,23 +1,3 @@
-// need global var for the output text field
-
-// function println(msg) {
-//   // print a message to the text field and the console
-//   output_field.value += msg + '\n';
-//   console.log(msg);
-// }
-//
-// function clear_text() {
-//   // clear the text field
-//   output_field.value = "";
-// }
-
-
-// used in mandelbrot, but probably very inefficient. does not use paths.
-// function fillPixel(x, y, c) {
-// 	ctx.fillStyle = c;
-// 	ctx.fillRect(x, y, 1, 1);
-// }
-//
 class CanvasUtil {
   constructor(ctx, width, height, output_field) {
     this.ctx = ctx;
@@ -52,32 +32,32 @@ class CanvasUtil {
 
   drawRect(x, y, width, height, color) {
     // draw a rectangle at (x, y) with given width, height, and color
-  	this.ctx.beginPath();
+    this.ctx.beginPath();
     this.ctx.fillStyle = color;
-  	this.ctx.rect(x, y, width, height);
-  	this.ctx.closePath();
-  	this.ctx.fill();
+    this.ctx.rect(x, y, width, height);
+    this.ctx.closePath();
+    this.ctx.fill();
   }
 
   drawCircle(x, y, radius, color, width) {
     // darw a circle at (x, y) with given radius, color (stroke instead of fill), and width
-  	this.ctx.beginPath();
+    this.ctx.beginPath();
     this.ctx.lineWidth = width;
-  	this.ctx.strokeStyle = color;
-  	this.ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
-  	this.ctx.closePath();
-  	this.ctx.stroke();
+    this.ctx.strokeStyle = color;
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+    this.ctx.closePath();
+    this.ctx.stroke();
   }
 
   drawLine(x0, y0, x1, y1, color, width) {
     // draw a line segment from (x0, y0) to (x1, y1) with given color and width
-  	this.ctx.beginPath();
+    this.ctx.beginPath();
     this.ctx.lineWidth = width;
-  	this.ctx.strokeStyle = color;
-  	this.ctx.moveTo(x0, y0);
-  	this.ctx.lineTo(x1, y1);
-  	this.ctx.closePath();
-  	this.ctx.stroke();
+    this.ctx.strokeStyle = color;
+    this.ctx.moveTo(x0, y0);
+    this.ctx.lineTo(x1, y1);
+    this.ctx.closePath();
+    this.ctx.stroke();
   }
 
   clearCanvas(color) {
@@ -101,6 +81,6 @@ class CanvasUtil {
     y = evt.clientY - rect.top;
     // optionally print
     // document.getElementById("x-coord").value = x;
-  	// document.getElementById("y-coord").value = y;
+    // document.getElementById("y-coord").value = y;
   }
 }
