62a4e1253eb14fd8d5fc1e3004b1fea69842a0d4
Michael Bradford Williams <michael@tvtime.com>
2020-01-13 08:18:40 -0800

    refactor vectors and particles

--- js/vectors.js
+++ js/vectors.js
@@ -1,209 +1,206 @@
-var x = 1;
-var y = 1;
+// TODO: fix vector length issues, fix global gravity case, fix sep selection
+
+var canvas;
+var ctx;
+var WIDTH = 750;
+var HEIGHT = 750;
+var diag = Math.sqrt(WIDTH * WIDTH + HEIGHT * HEIGHT);
+var canvasUtil;
+var vf; // VectorField
+var mousePos = new Vec2D(0, 0);
+
 var M = 4000;
-
-var x0, y0;
-
-var p_x;
-var p_y;
-var v_x;
-var v_y;
-var a_x;
-var a_y;
-var r_squared;
-var rad;
-
 var m = 1;
 var G = 1/10;
-var grav = 1;
-var a_g = 2;
+var grav = 0;
+var acc_g = new Vec2D(0.0, -1.0);
 var K = 3;
 
 var sep = 50;  // separation of vectors (vertical and horizontal)
 var mode = 1;
 var away = 1;
 
-var canvas, ctx;
-var WIDTH = 750;
-var HEIGHT = 750;
-var width = 750; // TODO: fix this
-var height = 750;
-var diag = Math.sqrt(width*width + height*height);  // diagonal length of canvas
-var canvasUtil;
 
+class GraphicalVector {
+  constructor(p0, p1, vel, color) {
+    this.p0 = p0; // Vec2D representing basepoint of segment
+    this.p1 = p1; // Vec2D representing endpoint of segment
+    this.vel = vel // Vec2D
+    this.color = color;
+  }
 
-function setupArrays() {
-  rad = 0.9*sep;
+  draw () {
+    // line segment for the vector, with small circle at base
+    canvasUtil.drawLine(this.p0.x, this.p0.y, this.p1.x, this.p1.y, this.color, 1);
+    canvasUtil.drawCircle(this.p0.x, this.p0.y, 2, this.color, 1);
+  }
+}
 
-  x0 = new Array(width/sep + 3);
-  y0 = new Array(width/sep + 3);
-  p_x = new Array(width/sep + 3);
-  p_y = new Array(width/sep + 3);
-  v_x = new Array(width/sep + 3);
-  v_y = new Array(width/sep + 3);
-  a_x = new Array(width/sep + 3);
-  a_y = new Array(width/sep + 3);
-  sin_theta = new Array(width/sep + 3);
-  cos_theta = new Array(width/sep + 3);
-  r_squared = new Array(width/sep + 3);
+class VectorField {
+  constructor(numRows, numCols) {
+    this.numRows = numRows;
+    this.numCols = numCols;
+    this.vectors;
+  }
 
-  for(var i=0; i<width/sep + 3; i++) {
-    x0[i] = new Array(height/sep + 3);
-    y0[i] = new Array(height/sep + 3);
-    p_x[i] = new Array(height/sep + 3);
-    p_y[i] = new Array(height/sep + 3);
-    v_x[i] = new Array(height/sep + 3);
-    v_y[i] = new Array(height/sep + 3);
-    a_x[i] = new Array(height/sep + 3);
-    a_y[i] = new Array(height/sep + 3);
-    sin_theta[i] = new Array(height/sep + 3);
-    cos_theta[i] = new Array(height/sep + 3);
-    r_squared[i] = new Array(height/sep + 3);
+  toString() {
+    let s = "";
+    for (let i=0; i<this.numRows; i++) {
+      for (let j=0; j<this.numCols; j++) {
+        s += this.vectors[i][j].p0.toString() + "->" + this.vectors[i][j].p1.toString() + '\n';
+      }
+    }
+    return s;
+  }
 
-    for(var j=0; j < height/sep + 3; j++) {
-      x0[i][j] = i*sep-sep;
-      y0[i][j] = j*sep-sep;
-      p_x[i][j] = x0[i][j]+rad;
-      p_y[i][j] = y0[i][j];
-      v_x[i][j] = 0;
-      v_y[i][j] = 0;
-      a_x[i][j] = 0;
-      a_y[i][j] = 0;
-      sin_theta[i][j] = 0;
-      cos_theta[i][j] = 0;
-      r_squared[i][j] = 0;
+  initialize() {
+    //canvasUtil.println(`initializing VF with sep = ${sep}`);
+    this.vectors = new Array(this.numRows);
+    for (let i=0; i<this.numRows; i++) {
+      this.vectors[i] = new Array(this.numCols);
+      for (let j=0; j<this.numCols; j++) {
+        let p0 = new Vec2D(sep / 2 + i * sep, sep / 2 + j * sep);
+        let p1 = p0.scale(-1 * away * Math.min(sep / p0.norm(), 0.9)).plus(p0);
+        let vel = new Vec2D(0.0, 0.0);
+        this.vectors[i][j] = new GraphicalVector(p0, p1, vel, "black");
+        //canvasUtil.println(`added vector at (${i}, ${j}): ${this.vectors[i][j].p1.toString()}`)
+      }
+    }
+  }
+
+  draw() {
+    for (let i=0; i<this.numRows; i++) {
+      for (let j=0; j<this.numCols; j++) {
+        this.vectors[i][j].draw();
+      }
+    }
+  }
+
+  updateVectors(updateFn) {
+    for (let i=0; i<this.numRows; i++) {
+      for (let j=0; j<this.numCols; j++) {
+        this.vectors[i][j] = updateFn(this.vectors[i][j]);
+      }
     }
   }
 }
 
+
 function draw() {
   canvasUtil.clearCanvas();
+  let f;
+  away = document.getElementById("direction").value;
 
-  // if anything changed, update the variables and reset the arrays
-  if (away != document.getElementById("direction").value ||
-      sep  != document.getElementById("separation").value*1 ||
-    mode != document.getElementById("style").value*1) {
-
-    away = document.getElementById("direction").value;
-    sep  = document.getElementById("separation").value*1;
-    mode = document.getElementById("style").value*1;
-
-    setupArrays();
+  if (sep  != document.getElementById("separation").value * 1) {
+    //sep = document.getElementById("separation").value * 1;
+    //vf.initialize(sep);
+  }
+  let mode = document.getElementById("style").value * 1;
+  switch (mode) {
+    case 0:
+      f = trackMouse;
+      break;
+    case 1:
+      f = boundedForce;
+      break;
+    case 2:
+      f = gravityLike;
+      break;
   }
 
-  if (mode == 0) { // vectors of bounded length
-    for (var i = 0 - sep; i <= width + sep; i = i + sep) {
-      for (var j = 0 - sep; j <= height + sep; j = j + sep) {
-        var dist = Math.sqrt( (i-x)*(i-x) + (j-y)*(j-y) );
+  vf.updateVectors(f);
+  vf.draw();
+}
 
-        var color = 'rgb('  + Math.round(255-255*x/width) + ','
-                      + Math.round(255*dist/diag) + ','
-                      + Math.round(255*y/height) + ')';
+function trackMouse(gv) {
+  let dist = gv.p0.distance(mousePos);
+  let r = Math.round(255 - 255 * dist / diag);
+  let g = 0;
+  let b = Math.round(255 * dist / diag);
+  let color = Color.colorString(r, g, b);
+  // (mouse - p0) * scale_factor + p0
+  let p1 = mousePos.minus(gv.p0).scale(away * Math.min(sep / dist, 0.9)).plus(gv.p0);
+  return new GraphicalVector(gv.p0, p1, gv.vel, color);
+}
 
-        canvasUtil.drawLine(i, j, i+(x-i)*Math.min(0.9*sep/dist,1/2)*away, j+(y-j)*Math.min(0.9*sep/dist,1/2)*away, color, 1);
-        canvasUtil.drawCircle(i, j, 2, color, 1);
-      }
-    }
-  } else if (mode == 1) { // vectors that remember their last direction (when outside range of mouse)
-    for (var i=0; i < width/sep + 3; i++) {
-      for (var j=0; j < height/sep + 3; j++) {
-        var color = "";
-        r_squared[i][j] = (p_x[i][j]-x)*(p_x[i][j]-x) + (p_y[i][j]-y)*(p_y[i][j]-y);
-        if( r_squared[i][j] < K*K*sep*sep ) {
+function gravityLike(gv) {
+  // forces from the mouse and gravity
+  let mouseMinusP0 = mousePos.minus(gv.p0);
+  let distSq = mouseMinusP0.normSq();
+  let dist = Math.sqrt(distSq);
+  let acc = mouseMinusP0.scale(away * G * M / distSq);//.plus(acc_g.scale(grav));
+  let accNormSq = acc.normSq();
+  let vel = acc; //let newVel = gv.vel.plus(acc);
+  let p1 = gv.p1.plus(vel); // direction of endpoint
 
-          // forces from the mouse and gravity
-          a_x[i][j] = (G*M/r_squared[i][j] - 1/(K*K*sep*sep))*(x-p_x[i][j])*away;
-          a_y[i][j] = (G*M/r_squared[i][j] - 1/(K*K*sep*sep))*(y-p_y[i][j])*away;
+  // project the endpoint onto a sphere around the base point
+  p1 = p1.minus(gv.p0).scale(500 / p1.minus(gv.p0).normSq()).plus(gv.p0); // 500???
+  let r = Math.round((255 * 2 / Math.PI) * Math.atan(accNormSq));
+  let g = 0;
+  let b = Math.round(255 - (255 * 2 / Math.PI) * Math.atan(accNormSq));
+  var color = Color.colorString(r, g, b);
+  return new GraphicalVector(gv.p0, p1, vel, color);
+}
 
-          // take velocities into account
-          v_x[i][j] += a_x[i][j];
-          v_y[i][j] += a_y[i][j];
 
-          // endpoint of vector
-          var x1 = p_x[i][j] + v_x[i][j];
-          var y1 = p_y[i][j] + v_y[i][j];
+function boundedForce(gv) {
+  let mouseMinusP0 = mousePos.minus(gv.p0);
+  let distSq = mouseMinusP0.normSq();
+  let dist = Math.sqrt(distSq);
+  let boundSq = K * K * sep * sep;
 
-          // project the endpoint onto a sphere around the base point
-          p_x[i][j] = Math.min(rad,Math.sqrt(r_squared[i][j]))*(x1 - x0[i][j])
-                /Math.sqrt( (x1-x0[i][j])*(x1-x0[i][j])  + (y1-y0[i][j])*(y1-y0[i][j]) ) + x0[i][j];
-          p_y[i][j] = Math.min(rad,Math.sqrt(r_squared[i][j]))*(y1 - y0[i][j])
-                /Math.sqrt( (x1-x0[i][j])*(x1-x0[i][j]) + (y1-y0[i][j])*(y1-y0[i][j]) ) + y0[i][j];
+  if (distSq < boundSq) {
+    // forces from the mouse and gravity
+    let acc = mouseMinusP0.scale(away * G * M / distSq - 1 / boundSq);
+    let vel = gv.vel.plus(acc);
+    let p1 = gv.p1.plus(vel); // direction of endpoint
 
-          color = 'rgb('  + Math.round( 255-255*r_squared[i][j]/(K*K*sep*sep))  + ','
-                        + 0 + ','
-                        + Math.round( 255*r_squared[i][j]/(K*K*sep*sep) ) + ')';
-        }
-        else { // far from mouse, so there is no velocity
-          v_x[i][j] = 0;
-          v_y[i][j] = 0;
-
-          color = 'rgb(' + 0 + ',' + 0 + ',' + Math.round( 255*K*K*sep*sep/r_squared[i][j] )  + ')';
-        }
-
-        canvasUtil.drawCircle(x0[i][j], y0[i][j], 2, color, 1);
-        canvasUtil.drawLine(x0[i][j], y0[i][j], p_x[i][j], p_y[i][j], color, 1);
-      }
-    }
-  } else {
-    grav = mode - 2;  // gravity on if mode = 3, off if mode = 2
-    for (var i=0; i < width/sep + 3; i++) {
-      for (var j=0; j < height/sep + 3; j++) {
-        // forces from the mouse and gravity
-        r_squared[i][j] = (p_x[i][j]-x)*(p_x[i][j]-x) + (p_y[i][j]-y)*(p_y[i][j]-y);
-        a_x[i][j] = G*(M/r_squared[i][j])*(x-p_x[i][j])*away;
-        a_y[i][j] = G*(M/r_squared[i][j])*(y-p_y[i][j])*away + grav*a_g;
-
-        // take velocities into account
-        v_x[i][j] = a_x[i][j];
-        v_y[i][j] = a_y[i][j];
-
-        // endpoint of vector
-        var x1 = p_x[i][j] + v_x[i][j];
-        var y1 = p_y[i][j] + v_y[i][j];
-
-        // project the endpoint onto a sphere around the base point
-        p_x[i][j] = Math.min(rad,Math.sqrt(r_squared[i][j]))*(x1 - x0[i][j])
-              /Math.sqrt( (x1-x0[i][j])*(x1-x0[i][j]) + (y1-y0[i][j])*(y1-y0[i][j]) ) + x0[i][j];
-        p_y[i][j] = Math.min(rad,Math.sqrt(r_squared[i][j]))*(y1 - y0[i][j])
-              /Math.sqrt( (x1-x0[i][j])*(x1-x0[i][j]) + (y1-y0[i][j])*(y1-y0[i][j]) ) + y0[i][j];
-
-        var a_norm_squared = a_x[i][j]*a_x[i][j] + (a_y[i][j]-grav*a_g)*(a_y[i][j]-grav*a_g)
-        var color = 'rgb('  + Math.round( (255*2/Math.PI)*Math.atan(a_norm_squared) ) + ','
-                      + 0 + ','
-                      + Math.round( 255-(255*2/Math.PI)*Math.atan(a_norm_squared) ) + ')';
-
-        canvasUtil.drawCircle(x0[i][j], y0[i][j], 2, color);
-        canvasUtil.drawLine(x0[i][j], y0[i][j], p_x[i][j], p_y[i][j], color, 1);
-      }
-    }
+    // project the endpoint onto a sphere around the base point
+    p1 = p1.minus(gv.p0).scale(Math.min(0.9 * sep, dist) / p1.minus(gv.p0).norm()).plus(gv.p0);
+    let r = Math.round(255 - 255 * distSq / boundSq);
+    let g = 0;
+    let b = Math.round(255 * distSq / boundSq);
+    let color = Color.colorString(r, g, b);
+    return new GraphicalVector(gv.p0, p1, vel, color);
+  }
+  else { // far from mouse, so there is no velocity
+    let r = 0;
+    let g = 0;
+    let b = Math.round(255 * boundSq / distSq);
+    let color = Color.colorString(r, g, b);
+    return new GraphicalVector(gv.p0, gv.p1, new Vec2D(0, 0), color);
   }
 }
 
 
 function ev_mousemove (ev) {
   const rect = canvas.getBoundingClientRect()
-  x = ev.clientX - rect.left
-  y = ev.clientY - rect.top
-  document.getElementById("x-coord").value = x;
-  document.getElementById("y-coord").value = y;
+  let mouseX = ev.clientX - rect.left
+  let mouseY = ev.clientY - rect.top
+  mousePos.x = mouseX;
+  mousePos.y = mouseY;
+  document.getElementById("x-coord").value = mouseX;
+  document.getElementById("y-coord").value = mouseY;
 }
 
 
 function init() {
   canvas = document.getElementById("canvas");
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
   if (canvas.getContext){
     ctx = canvas.getContext('2d');
-    canvasUtil = new CanvasUtil(ctx, WIDTH, HEIGHT);
+    canvasUtil = new CanvasUtil(ctx, WIDTH, HEIGHT, document.outform.output);
     canvas.addEventListener('mousemove', ev_mousemove, false);
-
-    x0 = width/2;
-    y0 = height/2;
-    setupArrays();
-
+    canvasUtil.clearCanvas();
+    let n = 33;
+    vf = new VectorField(n, n);
+    sep = WIDTH / n
+    vf.initialize(WIDTH / n);
+    //canvasUtil.println(vf.toString());
+    vf.draw();
     return setInterval(draw, 10);
-  } else { 
+  } else {
 		alert('You need a better web browser to see this.');
 	}
 }
