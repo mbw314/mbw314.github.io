ab7515bcea02f33ddd31c0e9ab3708e4103e5fe5
mbw314 <mbw314@gmail.com>
2020-01-13 09:36:39 -0800

    Develop (#17)
    * Refactor part 2 (#7)
    
    * use drawing functions from CanvasUtil
    
    * use println from CanvasUtil; refactor particles
    
    * minor cleanup
    
    * more cleanup
    
    * more cleanup
    
    * replace tabs with spaces
    
    * Add template (#9)
    
    * add template html and js files
    
    * cleanup
    
    * Develop (#10) (#11)
    
    * Refactor part 2 (#7)
    
    * use drawing functions from CanvasUtil
    
    * use println from CanvasUtil; refactor particles
    
    * minor cleanup
    
    * more cleanup
    
    * more cleanup
    
    * replace tabs with spaces
    
    * Add template (#9)
    
    * add template html and js files
    
    * cleanup
    
    * Refactor part 3 (#12)
    
    * refactor complex mappings; reformat automata
    
    * formatting
    
    * fix toString
    
    * Refactor part 4 (#14)
    
    * refactor automata
    
    * change colormatrix
    
    * refactor vectors and particles (#16)
    
    * refactor vectors and particles
    
    * add comments; remove old code

--- js/canvasUtil.js
+++ js/canvasUtil.js
@@ -40,7 +40,7 @@ class CanvasUtil {
   }
 
   drawCircle(x, y, radius, color, width) {
-    // darw a circle at (x, y) with given radius, color (stroke instead of fill), and width
+    // draw a circle at (x, y) with given radius, color (stroke instead of fill), and width
     this.ctx.beginPath();
     this.ctx.lineWidth = width;
     this.ctx.strokeStyle = color;
@@ -71,5 +71,169 @@ class CanvasUtil {
     this.ctx.rect(0, 0, this.width, this.height);
     this.ctx.closePath();
     this.ctx.fill();
+  }
+}
+
+
+class Color {
+  // triple of numbers representing an RGB color
+  // useful for constructing colors programatically
+  constructor(red, green, blue) {
+    this.r = Math.round(red);
+    this.g = Math.round(green);
+    this.b = Math.round(blue);
+  }
+
+  static interpolate(c0, c1, t) {
+    // if a color is a point in [0, 255] x [0, 255] x [0, 255], parametrize a line segment
+    // between two colors by [0, 1], that is, c(t) = t * c1 + (1 - t) * c0,
+    // and find the color c(t) at given parameter t.
+    let r = (t * c1.r + (1.0 - t) * c0.r) % 256;
+    let g = (t * c1.g + (1.0 - t) * c0.g) % 256;
+    let b = (t * c1.b + (1.0 - t) * c0.b) % 256;
+    return new Color(r, g, b);
+  }
+
+  static colorString(r, g, b) {
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  static random() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return Color.colorString(r, g, b);
+  }
+
+  toString() {
+    return `rgb(${this.r}, ${this.g}, ${this.b})`;
+  }
+}
+
+
+class Point {
+  constructor(x, y) {
+    this.x = x; // float
+    this.y = y; // float
+  }
+
+  translate(dir, dist) { // vec2d and float
+    let unit_dir = dir.scale(1 / dir.norm());
+    let new_x = this.x + unit_dir.x * dist;
+    let new_y = this.y + unit_dir.y * dist;
+    return new Point(new_x, new_y);
+  }
+
+  toString() {
+    return ['(', this.x.toFixed(3), ', ', this.y.toFixed(3), ')'].join('');
+  }
+
+  distanceSq(p) {
+    return (this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y);
+  }
+
+  distance(p) {
+    return Math.sqrt(this.distanceSq(p));
+  }
+
+
+
+  scale(a) {
+    return new Vec2D(a * this.x, a * this.y);
+  }
+
+  static centroid(points) { // list of Point objects
+    let centroid_x = 0.0;
+    let centroid_y = 0.0;
+    for (let i in points) {
+      centroid_x += points[i].x;
+      centroid_y += points[i].y;
+      //console.log('new centroid vals: ' + centroid_x + ', ' + centroid_y);
+    }
+    return new Point(centroid_x / points.length, centroid_y / points.length);
+  }
+}
+
+
+class LineSegment {
+  constructor(initial, terminal, width, color) {
+    this.p0 = initial // Point
+    this.p1 = terminal // Point
+    this.width = width // int
+    this.color = color // string
+  }
+
+  draw(canvasUtil) { // canvas context
+    canvasUtil.drawLine(
+      this.p0.x,
+      this.p0.y,
+      this.p1.x,
+      this.p1.y,
+      this.color,
+      this.width);
+  }
+
+  toString() {
+    return [
+      this.p0.toString(), '->', this.p1.toString(),
+      '; color =', this.color,
+      '; width =', this.width].join(' ');
+  }
+
+  getMidpoint() {
+    let x = (this.p0.x + this.p1.x) / 2.0;
+    let y = (this.p0.y + this.p1.y) / 2.0;
+    return new Point(x, y);
+  }
+
+  scale(a) {
+    this.p0 = this.p0.scale(a);
+    this.p1 = this.p1.scale(a);
+  }
+
+  translate(dir, dist) {
+    this.p0 = this.p0.translate(dir, dist);
+    this.p1 = this.p1.translate(dir, dist);
+  }
+}
+
+
+class Vec2D extends Point {
+  constructor(x, y) {
+    super(x, y);
+  }
+
+  plus(v) {
+    return new Vec2D(this.x + v.x, this.y + v.y);
+  }
+
+  minus(v) {
+    return new Vec2D(this.x - v.x, this.y - v.y);
+  }
+
+  rotate(theta) { // radians
+    let new_x = this.x * Math.cos(theta) - this.y * Math.sin(theta);
+    let new_y = this.x * Math.sin(theta) + this.y * Math.cos(theta);
+    return new Vec2D(new_x, new_y);
+  }
+
+  normSq() {
+    return this.x * this.x + this.y * this.y;
+  }
+
+  norm() {
+    return Math.sqrt(this.normSq());
+  }
+
+  dot(v) {
+    return this.x * v.x + this.y * v.y;
+  }
+
+  toString() {
+    return ['(', this.x.toFixed(3), ', ', this.y.toFixed(3), ')'].join('')
+  }
+
+  toUnitVector() {
+    return this.scale(1.0 / this.norm());
   }
 }
