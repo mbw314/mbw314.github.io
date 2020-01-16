a2b751cb3b7dcc0d407e9d01c00d366e456f472d
mbw314 <mbw314@gmail.com>
2020-01-15 22:28:04 -0800

    Refactor part 7 (#24) (#25)
    * refactor strings
    
    * rename to particles; add width
    
    * improve refactor of kmeans

--- js/kmeans.js
+++ js/kmeans.js
@@ -4,6 +4,7 @@ let canvas;
 let ctx;
 let canvasUtil;
 let kme; // k-means ensemble
+const BLACK = Color.colorString(0, 0, 0);
 const NUM_POINTS = 75;
 const POINT_RADIUS = 5;
 const NUM_CLUSTERS = 5;
@@ -11,134 +12,118 @@ const CENTROID_RADIUS = 15;
 const OLD_CENTROID_COLOR = "gray";
 
 
-class KMeansEnsemble {
-  constructor(numPoints, numClusters) {
-    this.numPoints = numPoints;
-    this.pointRadius = POINT_RADIUS;
-    this.numClusters = numClusters;
-    this.centroidRadius = CENTROID_RADIUS;
-    this.points = []; // array of Vec2D
-    this.pointColors = []; // array of string
-    this.centroids = []; // array of Vec2D
-    this.clusterColors = []; // array of string
-    this.clusterIndexes = []; // array of int
-    this.oldCentroids = []; // array of arrays of Vec2D
-    this.oldCentroidColor = OLD_CENTROID_COLOR;
+class KMeansPoint {
+  constructor(pos, color, clusterIndex) {
+    this.pos = pos; // Point
+    this.color = color; // String
+    this.radius = POINT_RADIUS;
+    this.clusterIndex = 0;
   }
 
-  toString() {
-    let clusters = `${this.numClusters} centroids: ${this.centroids.map(c => c.toString()).join(', ')}`;
-    let clusterColors = `${this.numClusters} cluster colors: ${this.clusterColors.join(', ')}`;
-    let points = `${this.numPoints} points: ${this.points.map(p => p.toString()).join(', ')}`;
-    return [clusters, clusterColors, points].join('\n');
+  draw() {
+    canvasUtil.drawDisk(this.pos.x, this.pos.y, this.radius, this.color);
+  }
+}
+
+class KMeansCentroid {
+  constructor(pos, color, radius, index) {
+    this.pos = pos; // Point
+    this.color = color;
+    this.radius = radius;
+    this.index = index;
+    this.history = []; // Array of Point objects
   }
 
-  initPoints() {
-    this.points = [];
-    this.clusterIndexes = [];
-    this.pointColors = [];
-    for (let i=0; i<this.numPoints; i++) {
-      this.points.push(new Vec2D(Math.random() * WIDTH, Math.random() * HEIGHT));
-      this.clusterIndexes.push(0);
-      this.pointColors.push("black");
-    }
-  }
-
-  drawPoints() {
-    for (let i=0; i<this.numPoints; i++) {
-      //canvasUtil.println(`drawing point at ${this.points[i].toString()} with color ${this.pointColors[i]}`);
-      canvasUtil.drawDisk(
-        this.points[i].x,
-        this.points[i].y,
-        this.pointRadius,
-        this.pointColors[i]
+  draw() {
+    // draw the history points and lines connecting them
+    for (let j=0; j<this.history.length-1; j++) {
+      canvasUtil.drawLine(
+        this.history[j].x,
+        this.history[j].y,
+        this.history[j+1].x,
+        this.history[j+1].y,
+        OLD_CENTROID_COLOR,
+        1
       );
-    }
-  }
-
-  initCentroids() {
-    this.centroids = [];
-    this.clusterColors = [];
-    this.oldCentroids = [];
-    for (let k=0; k<this.numClusters; k++) {
-      this.centroids.push(new Vec2D(Math.random() * WIDTH, Math.random() * HEIGHT));
-      let r = Math.round(k * 255 / this.numClusters);
-      let g = Math.round((this.numClusters - k) * 255 / this.numClusters);
-      let b = Math.round(Math.random() * 255);
-      this.clusterColors.push(Color.colorString(r, g, b));
-      this.oldCentroids.push([]);
-    }
-  }
-
-  drawCentroids() {
-    for (let k=0; k<this.numClusters; k++) {
-      //canvasUtil.println(`drawing cluster center ${k+1} of ${this.numClusters} at ${this.centroids[k].toString()} with color ${this.clusterColors[k]}`);
       canvasUtil.drawCircle(
-        this.centroids[k].x,
-        this.centroids[k].y,
-        this.centroidRadius,
-        this.clusterColors[k],
-        2
+        this.history[j].x,
+        this.history[j].y,
+        this.radius / 2,
+        OLD_CENTROID_COLOR,
+        1
       );
     }
+    // optionally draw a segment from the last old centroid the the current one
+    if (this.history.length > 0) {
+      canvasUtil.drawLine(
+        this.history[this.history.length-1].x,
+        this.history[this.history.length-1].y,
+        this.pos.x,
+        this.pos.y,
+        OLD_CENTROID_COLOR,
+        1
+      );
+    }
+    // draw the centroid itself
+    canvasUtil.drawCircle(this.pos.x, this.pos.y, this.radius, this.color);
+  }
+}
+
+class KMeansEnsemble2 {
+  constructor() {
+    this.points = []; // Array of KMeansPoint objects
+    this.centroids = []; // Array of KMeansCentroid objects
+    this.numPoints = 0;
+    this.numCentroids = 0;
   }
 
-  initialize() {
+  initialize(numPoints, numClusters) {
+    this.initPoints(numPoints);
+    this.initCentroids(numClusters);
     canvasUtil.clearCanvas();
-    this.initPoints();
-    this.initCentroids();
     this.drawPoints();
     this.drawCentroids();
   }
 
-  drawConnectors() {
-    for (let k=0; k<this.numClusters; k++) {
-      let cluster = getAllIndexes(this.clusterIndexes, k);
-      for (let j=0; j<cluster.length; j++) {
-        canvasUtil.drawLine(
-          this.centroids[k].x,
-          this.centroids[k].y,
-          this.points[cluster[j]].x,
-          this.points[cluster[j]].y,
-          'black',
-          1
-        );
-      }
+  initPoints(numPoints) {
+    this.numPoints = numPoints;
+    this.points = [];
+    for (let i=0; i<numPoints; i++) {
+      this.points.push(new KMeansPoint(new Vec2D(Math.random() * WIDTH, Math.random() * HEIGHT), BLACK, 0));
     }
   }
 
-  drawCentroidPaths() {
-    for (let k=0; k<this.numClusters; k++) {
-      for (let j=0; j<this.oldCentroids[k].length-1; j++) {
-        canvasUtil.drawLine(
-          this.oldCentroids[k][j].x,
-          this.oldCentroids[k][j].y,
-          this.oldCentroids[k][j+1].x,
-          this.oldCentroids[k][j+1].y,
-          this.oldCentroidColor,
-          1
-        );
+  drawPoints() {
+    this.points.forEach(p => p.draw());
+  }
 
-        canvasUtil.drawCircle(
-          this.oldCentroids[k][j].x,
-          this.oldCentroids[k][j].y,
-          this.centroidRadius / 2,
-          this.oldCentroidColor,
-          1
-        );
+  initCentroids(numCentroids) {
+    this.numCentroids = numCentroids;
+    this.centroids = [];
+    for (let k=0; k<numCentroids; k++) {
+      let p = new Vec2D(Math.random() * WIDTH, Math.random() * HEIGHT);
+      let r = Math.round(k * 255 / numCentroids);
+      let g = Math.round((numCentroids - k) * 255 / numCentroids);
+      let b = Math.round(Math.random() * 255);
+      let c = Color.colorString(r, g, b);
+      this.centroids.push(new KMeansCentroid(p, c, CENTROID_RADIUS, k));
+    }
+  }
 
-      }
-      // optionally draw a segment from the last old centroid the the current one
-      if (this.oldCentroids[k].length > 0) {
-        canvasUtil.drawLine(
-          this.oldCentroids[k][this.oldCentroids[k].length-1].x,
-          this.oldCentroids[k][this.oldCentroids[k].length-1].y,
-          this.centroids[k].x,
-          this.centroids[k].y,
-          this.oldCentroidColor,
-          1
-        );
-      }
+  drawCentroids() {
+    this.centroids.forEach(c => c.draw());
+  }
+
+  drawConnectors() {
+    for (let i=0; i<this.numPoints; i++) {
+      canvasUtil.drawLine(
+        this.centroids[this.points[i].clusterIndex].pos.x,
+        this.centroids[this.points[i].clusterIndex].pos.y,
+        this.points[i].pos.x,
+        this.points[i].pos.y,
+        BLACK,
+        1
+      );
     }
   }
 
@@ -148,54 +133,34 @@ class KMeansEnsemble {
       // for each data point, find the index of the closest centroid
       let minDistSq = WIDTH * WIDTH + HEIGHT * HEIGHT; // largest possible distance from a point to a centroid
       let indexOfClosest = 0;
-      for (let k=0; k<this.numClusters; k++) {
-        let distSq = this.points[i].distanceSq(this.centroids[k]);
+      for (let k=0; k<this.numCentroids; k++) {
+        let distSq = this.points[i].pos.distanceSq(this.centroids[k].pos);
         if (distSq < minDistSq) {
           indexOfClosest = k;
           minDistSq = distSq;
         }
       }
       // find index corresponding to smallest distance, color point accordingly
-      this.clusterIndexes[i] = indexOfClosest;
-      this.pointColors[i] = this.clusterColors[this.clusterIndexes[i]];
+      this.points[i].clusterIndex = indexOfClosest;
+      this.points[i].color = this.centroids[indexOfClosest].color;
     }
 
     // step 2: centroid movement
-    for (let k=0; k<this.numClusters; k++) {
-      this.oldCentroids[k].push(this.centroids[k]); // keep track of the previous position
-      let cluster = getAllIndexes(this.clusterIndexes, k); // indices of points belonging to cluster k
+    for (let k=0; k<this.numCentroids; k++) {
+      this.centroids[k].history.push(this.centroids[k].pos); // keep track of the previous position
+      let cluster = getAllIndexes(this.points.map(p => p.clusterIndex), k); // indices of points belonging to cluster k
       if (cluster.length != 0) {
         // replace centroid with the cluster average
-        let pointsInCluster = cluster.map(i => this.points[i]);
-        this.centroids[k] = Point.centroid(pointsInCluster);
+        let pointsInCluster = cluster.map(i => this.points[i].pos);
+        this.centroids[k].pos = Point.centroid(pointsInCluster);
       } else {
         // reassign the centroid randomly
-        this.centroids[k] = new Vec2D(Math.random() * WIDTH, Math.random() * HEIGHT);
+        this.centroids[k].pos = new Vec2D(Math.random() * WIDTH, Math.random() * HEIGHT);
       }
     }
   }
 }
 
-
-function indexOfSmallest(a) {
-  let lowest = 0;
-  for (let i=1; i < a.length; i++) {
-    if (a[i] < a[lowest]) {
-      lowest = i;
-    }
-  }
-  return lowest;
-}
-
-function countInArray(a, i) {
-  let result = 0;
-  for (let o in a) {
-    if (a[o] == i) {
-      result++;
-    }
-  }
-  return result;
-}
 
 function getAllIndexes(a, v) {
   let indexes = [];
@@ -209,7 +174,7 @@ function getAllIndexes(a, v) {
 
 function refreshData() {
   canvasUtil.clearCanvas();
-  kme.initialize();
+  kme.initialize(NUM_POINTS, NUM_CLUSTERS);
 }
 
 function iterate() {
@@ -217,7 +182,6 @@ function iterate() {
   kme.iterate();
   kme.drawConnectors();
   kme.drawCentroids();
-  kme.drawCentroidPaths();
   kme.drawPoints();
 }
 
@@ -228,7 +192,7 @@ function init() {
   if (canvas.getContext){
     ctx = canvas.getContext('2d');
     canvasUtil = new CanvasUtil(ctx, WIDTH, HEIGHT, document.outform.output);
-    kme = new KMeansEnsemble(NUM_POINTS, NUM_CLUSTERS);
+    kme = new KMeansEnsemble2();
     refreshData();
   }
   else {
