fb5e53c288b7c005af0046ff24317a5871af1944
mbw314 <mbw314@gmail.com>
2020-02-25 14:58:30 -0800

    refactor lorenz; add projections (#36)

--- js/lorenz.js
+++ js/lorenz.js
@@ -4,6 +4,7 @@ let canvas;
 let ctx;
 let canvasUtil;
 let paused = false;
+let curve; // ParametrizedCurve object
 
 let x0 = 1.0;
 let y0 = -1.4;
@@ -13,48 +14,12 @@ const RHO = 28;
 const BETA = 8 / 3.0;
 const T = 0.01;
 const MAX_POINTS = 5000;
-let points;// array of 2d points to draw
-var x;
-var y;
-var z;
 const BLUE = new Color(0, 0, 255);
 const RED = new Color(255, 0, 0);
 const WHITE = new Color(255, 255, 255);
 
-
-function resetDrawing() {
-  let x0 = 1.0 + Math.random() * 0.1;
-  let y0 = -1.4 + Math.random() * 0.1;;
-  let z0 = 0.6 + Math.random() * 0.1;;
-  points = new Array(MAX_POINTS).fill(spaceToScreen(x0, y0, z0));
-  [x, y, z] = lorenzUpdate(x0, y0, z0);
-  canvasUtil.clearCanvas();
-  canvasUtil.clearText();
-  canvasUtil.println(`initial position: (${x0.toFixed(3)}, ${y0.toFixed(3)}, ${z0.toFixed(3)})`);
-  canvasUtil.println(`parameters: sigma = ${SIGMA.toFixed(3)}, rho = ${RHO.toFixed(3)}, beta = ${BETA.toFixed(3)}`);
-}
-
-function lorenzUpdate(x, y, z) {
-  let dx = SIGMA * (y - x);
-  let dy = x * (RHO - z) - y;
-  let dz = x * y - BETA * z;
-  return [x + T * dx, y + T * dy, z + T * dz];
-}
-
-function pauseDrawing() {
-  paused = !paused;
-}
-
-function spaceToScreen(x, y, z) {
-  // project 3d space onto plane z=0;
-  let a = -32;
-  let b = 32;
-  let c = -32;
-  let d = 32;
-
-  let px = WIDTH * (x - a) / (b - a);
-  let py = HEIGHT * (d - y) / (d - c);
-  return [px, py];
+function colorFn(i) {
+  return Color.interpolate(BLUE, WHITE, i / MAX_POINTS);
 }
 
 function draw() {
@@ -62,19 +27,81 @@ function draw() {
     return 0;
   }
 
-  [x1, y1, z1] = lorenzUpdate(x, y, z);
-  let p0 = spaceToScreen(x, y, z);
-  let p1 = spaceToScreen(x1, y1, z1);
-  points = prepend(points, [p1[0], p1[1]]);
+  curve.updatePoints();
   canvasUtil.clearCanvas();
-  for (let i=0; i < MAX_POINTS-1; i++) {
-    let c = Color.interpolate(BLUE, WHITE, i / MAX_POINTS);
-    canvasUtil.drawLine(points[i][0], points[i][1], points[i+1][0], points[i+1][1], c, 1);
+  let proj = document.parameters.projection.value;
+  if (proj == "xy") {
+    curve.projectionFn = spaceToScreenXY;
+  } else if (proj == "xz") {
+    curve.projectionFn = spaceToScreenXZ;
+  } else if (proj == "yz") {
+    curve.projectionFn = spaceToScreenYZ;
   }
+  for (let i=0; i < MAX_POINTS-1; i++) {
+    let c = curve.colorFn(i);
+    let p0 = curve.projectionFn(curve.points[i]);
+    let p1 = curve.projectionFn(curve.points[i+1]);
+    canvasUtil.drawLine(p0.x, p0.y, p1.x, p1.y, c, 1);
+  }
+}
 
-  x = x1;
-  y = y1;
-  z = z1;
+function resetDrawing() {
+  let p0 = new Point3D(
+    x0 + Math.random() * 0.1,
+    y0 + Math.random() * 0.1,
+    z0 + Math.random() * 0.1);
+  curve = new ParametrizedCurve(p0, lorenzUpdate, spaceToScreenXY, colorFn);
+  canvasUtil.clearCanvas();
+  canvasUtil.clearText();
+  canvasUtil.println(`initial position: (${p0.x.toFixed(3)}, ${p0.y.toFixed(3)}, ${p0.z.toFixed(3)})`);
+  canvasUtil.println(`parameters: sigma = ${SIGMA.toFixed(3)}, rho = ${RHO.toFixed(3)}, beta = ${BETA.toFixed(3)}`);
+}
+
+function lorenzUpdate(p) {
+  let dx = SIGMA * (p.y - p.x);
+  let dy = p.x * (RHO - p.z) - p.y;
+  let dz = p.x * p.y - BETA * p.z;
+  return new Point3D(p.x + T * dx, p.y + T * dy, p.z + T * dz);
+}
+
+function pauseDrawing() {
+  paused = !paused;
+}
+
+function spaceToScreenXY(p) {
+  // project 3d space onto plane z=0, rescaled to canvas
+  let a = -32;
+  let b = 32;
+  let c = -32;
+  let d = 32;
+
+  let px = WIDTH * (p.x - a) / (b - a);
+  let py = HEIGHT * (d - p.y) / (d - c);
+  return new Point(px, py);
+}
+
+function spaceToScreenXZ(p) {
+  // project 3d space onto plane y=0, rescaled to canvas
+  let a = -32;
+  let b = 32;
+  let c = 0;
+  let d = 64;
+
+  let px = WIDTH * (p.x - a) / (b - a);
+  let pz = HEIGHT * (d - p.z) / (d - c);
+  return new Point(px, pz);
+}
+
+function spaceToScreenYZ(p) {
+  // project 3d space onto plane x=0, rescaled to canvas
+  let a = -32;
+  let b = 32;
+  let c = 0;
+  let d = 64;
+
+  let py = WIDTH * (p.y - a) / (b - a);
+  let pz = HEIGHT * (d - p.z) / (d - c);
+  return new Point(py, pz);
 }
 
 function prepend(a, item) {
@@ -84,7 +111,6 @@ function prepend(a, item) {
   a[0] = item;
   return a;
 }
-
 
 function init(adjustSize) {
   canvas = document.getElementById("canvas");
